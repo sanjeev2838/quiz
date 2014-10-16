dd568e82366495784325765ac5b4434b59f8ae2f
Sanjeev <sanjeevimpinge@gmail.com>
2014-10-16 15:49:39 +0530

    Initial commit for quiz Application

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.4'
+gem 'sqlite3'
+
+# Use SCSS for stylesheets
+gem 'sass-rails', '~> 4.0.2'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'therubyracer', platforms: :ruby
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+
+
+gem 'semantic-ui-sass', github: 'doabit/semantic-ui-sass'
+gem 'devise'
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
+gem 'debugger', group: [:development, :test]
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quiz::Application.config.secret_key_base = 'bc70f917547b1470e1927d93bb491913c5ca65e737891338b0695c5b87c8a9ac16fcc141f861f46f32c575f5a92678bac79d45f67e1d048c2002cfb8dc7c3f4f'
